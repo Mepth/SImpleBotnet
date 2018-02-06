65891474c912bf406544a5590abfe4182cf7ee19
Agatka <polietaiev2000@gmail.com>
2018-02-06 21:23:13 +0300

    Update

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 # !/usr/bin/python
 import socket
 ips = ['127.0.0.1:55579', '127.0.0.1:55578']
-for num in range(len(ips)):
-    ip_port = ips[num].split(':')
+for num in ips:
+    ip_port = ips.split(':')
     s = socket.socket()
     s.connect((ip_port[0], int(ip_port[1])))
     s.send('here you ddos command')
